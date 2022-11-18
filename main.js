329958b899f1d41b4fb6ecf5c9e5ee9044c697b7
gab <wr3gab@gmail.com>
2022-11-18 17:20:11 +0800

    modified the observer

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+const options = {
+	threshold: 1,
+}
+
 const observer = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('show')
-		} else {
-			entry.target.classList.remove('show')
-		}
+		entry.target.classList.toggle("show", entry.isIntersecting)
+
+		if (entry.isIntersecting) observer.unobserve(entry.target)
 	})
-})
+
+}, options)
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach(element => observer.observe(element))
